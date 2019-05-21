c974bcfe7da401f8d9295cc76eeb89426b8e63f9
Carlos Parra <carlos.parra97@gmail.com>
2019-05-20 22:47:53 -0400

    No conecta con el localtunnel

--- index.js
+++ index.js
@@ -4,26 +4,47 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const ipify = require('ipify');
+const customDomain = 'p2prueba'
+//var _ip = require('ip')
 
-const customDomain = 'ricardoasombrateporfis'
+function getLocalIp(){
+    let ip = _ip.address();
+    console.log('ip local de la araña: ' + `${ip}`);
 
-var tunnel = localtunnel(PORT, { subdomain: `${customDomain}`}, (err, tunnel) => {
-    console.log(tunnel);
+}
+
+function getPublicIp(){
+    (async () => {
+        let ip = await ipify()
+        console.log('ip publica de la araña: ' + `${ip}`);
+    })();
+}
+    
+
+var tunnel = localtunnel(PORT, { subdomain: `${customDomain}` }, (err, tunnel) => {
+    console.log('}');
+    console.log('Abierto el tunel');
+    console.log('}');
     tunnel.on('close', () => {
-        console.log("Se te cerró, ups")
+        console.log("Se te cerró, whoops")
     })
 })
 
+//Para servir archivos 
 app.use(express.static(__dirname + '/'))
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
+    //console.dir(req)
 });
 
 io.on('connection', function (socket) {
     console.log('nació una araña')
+    //getLocalIp();
+    //getPublicIp();
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        io.emit('chat message', msg)
     })
     socket.on('disconnect', function () {
         console.log('murió una araña')
@@ -32,4 +53,5 @@ io.on('connection', function (socket) {
 
 http.listen(PORT, function () {
     console.log(`listening on *:${PORT}`)
+
 })
