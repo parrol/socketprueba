a55aa36132b87d72796c64891674fce5209e6ba4
Carlos Parra <carlos.parra97@gmail.com>
2019-06-10 06:45:25 -0400

    Setting up froala editor

---
+++ realtime-froala-editor-master/public/sketch.js
@@ -0,0 +1,40 @@
+var socket;
+var text = {
+    text: ''
+};
+
+function setup(){
+    socket = io.connect('https://realtime-editor-endpoint.herokuapp.com/' || 'http://localhost:8080');
+    $("#text").on("froalaEditor.keyup", function(){
+        var html = $(this).froalaEditor('html.get');
+        var data = {
+        };
+        socket.emit('text', data);
+    });
+    $('#text').froalaEditor({
+        toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', '|', 'fontFamily', 'fontSize', 'color', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'selectAll', 'clearFormatting', '|', 'undo', 'redo'],
+        fullPage: true
+    });
+
+    socket.on('text', handleRecievedText);
+    socket.on('newUser', updateText);
+}
+
+function updateText(data){
+    text.text = data.text;
+    $("#text").froalaEditor('html.set', data.text);
+    var editor = $('#text').data('froala.editor');
+    editor.selection.setAtEnd(editor.$el.get(0));
+    editor.selection.restore();
+}
+
+function handleRecievedText(data){
+    console.log(data);
+    text.text = data.text;
+    $("#text").froalaEditor('html.set', data.text);
+    var editor = $('#text').data('froala.editor');
+    editor.selection.setAtEnd(editor.$el.get(0));
+    editor.selection.restore();
+}
+
+
